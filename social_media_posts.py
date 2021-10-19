8c7294c8c22cd876115f7bf1de4fa103a4ea5793
Caroline-xu <xuziyu7@gmail.com>
2021-10-19 14:41:28 -0400

    Update social_media_posts.py

--- social_media_posts.py
+++ social_media_posts.py
@@ -1,9 +1,15 @@
 import pandas as pd
 import re
+import nltk
 
+#Data Proprocessing
 df = pd.read_csv('500_Reddit_users_posts_labels.csv')
 pts = df['Post']
 pts.to_csv('posts.csv',index=False)
 pts.to_csv('posts.txt',index=False,sep='\t')
+#split it into words
+tokens = nltk.word_tokenize(pts)
+
+
 
 
