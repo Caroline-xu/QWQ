164a24af47ddf30a3a137a46b7d72780cc9e3fab
Caroline-xu <xuziyu7@gmail.com>
2021-10-20 21:26:24 -0400

    Update social_media_posts.py

--- social_media_posts.py
+++ social_media_posts.py
@@ -2,10 +2,10 @@ import pandas as pd
 import re
 import nltk
 from nltk.tokenize import WordPunctTokenizer
-nltk.download(’stopwords’)
+nltk.download('stopwords')
 from nltk.corpus import stopwords
 # needed for nltk.pos_tag function nltk.download(’averaged_perceptron_tagger’)
-nltk.download(’wordnet’)
+nltk.download('wordnet')
 from nltk.stem import WordNetLemmatizer
 
 #Data Proprocessing
@@ -13,21 +13,20 @@ df = pd.read_csv('500_Reddit_users_posts_labels.csv')
 pts = df['Post']
 pts.to_csv('posts.csv',index=False)
 pts.to_csv('posts.txt',index=False,sep='\t')
-# https://towardsdatascience.com/text-processing-in-python-29e86ea4114c
-#split the paragraph into lists of sentences (tokenization)
-word_punct_token = WordPunctTokenizer().tokenize(pts)
-#removed the tokens which are not a word (normalization)
+
+# the size of the data
+l = len(pts)
+# tokenization for each comment
 clean_token=[]
-for token in word_punct_token:
-    token = token.lower()
-    # remove any value that are not alphabetical
-    new_token = re.sub(r'[^a-zA-Z]+', '', token) 
-    # remove empty value and single character value
-    if new_token != "" and len(new_token) >= 2: 
-        vowels=len([v for v in new_token if v in "aeiou"])
-        if vowels != 0: # remove line that only contains consonants
-            clean_token.append(new_token)
-
-
-#split it into words
-#tokens = nltk.word_tokenize(pts)
+for i in range(l) :
+    word_punct_token = WordPunctTokenizer().tokenize(pts[i])
+    #removed the tokens which are not a word (normalization)
+    for token in word_punct_token:
+        token = token.lower()
+        # remove any value that are not alphabetical
+        new_token = re.sub(r'[^a-zA-Z]+', '', token) 
+        # remove empty value and single character value
+        if new_token != "" and len(new_token) >= 2: 
+            vowels=len([v for v in new_token if v in "aeiou"])
+            if vowels != 0: # remove line that only contains consonants
+                clean_token.append(new_token)
