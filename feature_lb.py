e6843f7566431d5be4b6d1d14be4f794ea6ff835
Wanqing Li <71136622+eus-lwq@users.noreply.github.com>
2021-11-03 19:35:42 -0400

    update feature extraction with sentiment

--- feature_lb.py
+++ feature_lb.py
@@ -40,6 +40,53 @@ compression_opts = dict(method='zip',
 df3.to_csv('countvec.zip', index=False,
           compression=compression_opts) 
 
+
+#Sentiment extraction
+#polarity of sentences
+def sentiment_scores(sentence): 
+    #initialize polarity of now sentence
+    polarity = 0
+    # Create a SentimentIntensityAnalyzer object. 
+    sid_obj = SentimentIntensityAnalyzer() 
+  
+    # polarity_scores method of SentimentIntensityAnalyzer 
+    # oject gives a sentiment dictionary. 
+    # which contains pos, neg, neu, and compound scores. 
+    sentiment_dict = sid_obj.polarity_scores(sentence) 
+    
+    #get rate of polarity of one post
+    this_negative = sentiment_dict['neg']
+    this_neural = sentiment_dict['neu']
+    this_positive = sentiment_dict['pos']
+    
+    # decide sentiment as positive, negative and neutral 
+    if sentiment_dict['compound'] >= 0.05 : 
+        #positive
+        polarity = 1 
+    elif sentiment_dict['compound'] <= - 0.05 : 
+        #negative
+        polarity = -1
+    else: 
+        #neural
+        polarity = 0
+    return polarity
+    
+def getPolarity():
+    df = pd.read_csv('500_Reddit_users_posts_labels.csv')
+    polarity_score = []
+    #df["Post"] = df.apply(axis=1)
+    for i in range(500):
+        this_score = sentiment_scores(df["Post"][i])
+        polarity_score.append(this_score)
+        print("index", i, "sentiment polarity:", this_score,"\n")
+    return polarity_score
+    
+polarity_score = getPolarity()
+print(polarity_score)
+df['Polarity'] = polarity_score
+print(df)
+
+
 #n-gram
 all_ngrams = []
 for sent in reddit_lemm:
