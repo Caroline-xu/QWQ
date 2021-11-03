e94b1b4dd7ce5f713e63b07daf2952c92b15e1f5
AmberWJL <71000902+AmberWJL@users.noreply.github.com>
2021-11-03 16:58:52 -0400

    Add files via upload
    random forest classification method:
     - take 4 parameters (X train, Y train, X test, Y test)

---
+++ random_forest.py
@@ -0,0 +1,19 @@
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.model_selection import train_test_split
+
+
+# split into train and test, X is features and Y is the label 
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# take four parameters, X is the independent variables(features), Y is the dependent varible(label)
+def random_forest(X_train, X_test, y_train, y_test):
+    # train algorithm
+    regressor = RandomForestRegressor(n_estimators=20, random_state=0)
+    regressor.fit(X_train, y_train)
+    y_pred = regressor.predict(X_test)
+
+    # evaluating the accuracy 
+    print(confusion_matrix(y_test,y_pred))
+    print(classification_report(y_test,y_pred))
+    print(accuracy_score(y_test, y_pred))
