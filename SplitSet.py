1eb76adcef72fe3da5b90439c8838bdeda4ea169
AmberWJL <71000902+AmberWJL@users.noreply.github.com>
2021-11-11 11:39:25 -0500

    Update SplitSet.py
    Correct Spelling mistake

--- SplitSet.py
+++ SplitSet.py
@@ -4,7 +4,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
 from matplotlib import pyplot as plt
-from train_linear_regression import train_linear_regression
+# from train_linear_regression import train_linear_regression
 # train_linear_regression() is defined in linear_regression.py
 #from linear_regression import train_linear_regression
 #from decision_tree import train_decision_tree
@@ -31,7 +31,7 @@ def get_data():
     df = feature_lb()
     X = df.loc[:, df.columns != 'New Label']
     y = df['New Label']
-    final_df = PCA_reduce_dimention(X,y)
+    final_df = PCA_reduce_dimension(X,y)
     #print(df)
     return final_df
 
@@ -45,13 +45,15 @@ if __name__ == '__main__':
     X = df.loc[:, df.columns != 'New Label']
     #y is label column
     y = df['New Label']
+
     """
     #####train model for only once###
     #split data into 0.8 train, 0.2 test
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
     model_name = 'linear_regression'
     res = train_linear_regression(X_train, X_test, y_train, y_test)
-    print("linear regression's accuracy:", res) """
+    print("linear regression's accuracy:", res) 
+    """
     
     ####train model by using 5 fold cross validation####
     # 2- Cross-fold validation on the train data
@@ -59,19 +61,28 @@ if __name__ == '__main__':
     kf = KFold(n_splits=k, random_state=None)
     acc_score = []
 
+
+    # random forest calssification 
     for train_index , test_index in kf.split(X):
         X_train , X_test = X.iloc[train_index,:],X.iloc[test_index,:]
         y_train , y_test = y[train_index] , y[test_index]
+        train_random_forest(X_train, X_test, y_train, y_test)
         
-        ### comment out the following line to train random forest classfication, and print out the accuracy 
-        # train_random_forest(X_train, X_test, y_train, y_test)
-        
+
+
+''' Linear Regression model
+    for train_index , test_index in kf.split(X):
+        X_train , X_test = X.iloc[train_index,:],X.iloc[test_index,:]
+        y_train , y_test = y[train_index] , y[test_index]
         acc = train_linear_regression(X_train, X_test, y_train, y_test)
         acc_score.append(acc)
     avg_acc_score = sum(acc_score)/k
     print('accuracy of each fold - {}'.format(acc_score))
     print('Avg accuracy : {}'.format(avg_acc_score))
-    
+'''
+
+
+
 # 3- Train a model
    #by changing this model name to get different model
 
