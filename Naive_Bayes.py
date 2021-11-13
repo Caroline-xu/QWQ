c404b15a4ad4469604bcfd54d1b4c8c699de1728
Wanqing Li <71136622+eus-lwq@users.noreply.github.com>
2021-11-12 19:18:58 -0500

    update function return

--- Naive_Bayes.py
+++ Naive_Bayes.py
@@ -1,10 +1,10 @@
 def train_naive_bayes(X_train, X_test, y_train, y_test):
-
+    
     from sklearn import datasets
     from sklearn.naive_bayes import GaussianNB
     import pandas as pd
     import numpy as np
-
+    from sklearn.metrics import mean_squared_error, mean_absolute_error, accuracy_score
     #calculate the GaussianNB
     gnb=GaussianNB()
 
@@ -13,10 +13,15 @@ def train_naive_bayes(X_train, X_test, y_train, y_test):
     # evaluate the model
     y_pred = gnb.predict(X_test)
     acc = np.sum(y_test == y_pred) / X_test.shape[0]
-    print("Naive Bayes Test Acc : %.3f" % acc)
+    #print("Test Acc : %.3f" % acc)
 
     # predict the model
     y_proba = gnb.predict_proba(X_test[:1])
     #print(gnb.predict(X_test[:1]))
     #print("The estimated probability:", y_proba)
-    #return acc
+    
+    acc = accuracy_score(y_test,y_pred)# need to change after this
+    mse = mean_squared_error(y_test,y_pred)
+    mae = mean_absolute_error(y_test, y_pred)
+    rmse = np.sqrt(mae)
+    return acc, mse, mae, rmse
