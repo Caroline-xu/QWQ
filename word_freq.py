c6fe7f1d2fc0c6280553d6899fa18635c0c2240a
AmberWJL <71000902+AmberWJL@users.noreply.github.com>
2021-11-16 13:37:50 -0500

    frequency of words has polarity not 0 for labels

--- word_freq.py
+++ word_freq.py
@@ -1,10 +1,17 @@
 
 from preprocessing import tokenized
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import nltk
+#nltk.download('vader_lexicon')
 import pandas as pd
 
 def wordListToFreqDict(wordlist):
-    wordfreq = [wordlist.count(p) for p in wordlist]
-    return dict(zip(wordlist,wordfreq))
+    somewords = []
+    for p in wordlist:
+        if sentiment_scores(p) != 0:
+            somewords.append(p)
+    wordfreq = [somewords.count(i) for i in somewords]
+    return dict(zip(somewords,wordfreq))
 
 def sortFreqDict(freqdict):
     freqDic = [(freqdict[key], key) for key in freqdict]
@@ -12,13 +19,40 @@ def sortFreqDict(freqdict):
     freqDic.reverse()
     return freqDic
 
+def sentiment_scores(sentence): 
+    #initialize polarity of now sentence
+    polarity = 0
+    # Create a SentimentIntensityAnalyzer object. 
+    sid_obj = SentimentIntensityAnalyzer()
+
+    # polarity_scores method of SentimentIntensityAnalyzer 
+    # oject gives a sentiment dictionary. 
+    # which contains pos, neg, neu, and compound scores. 
+    sentiment_dict = sid_obj.polarity_scores(sentence) 
+    
+    #get rate of polarity of one post
+    this_negative = sentiment_dict['neg']
+    this_neural = sentiment_dict['neu']
+    this_positive = sentiment_dict['pos']
+    
+    # decide sentiment as positive, negative and neutral 
+    if sentiment_dict['compound'] >= 0.05 : 
+        #positive
+        polarity = 1 
+    elif sentiment_dict['compound'] <= - 0.05 : 
+        #negative
+        polarity = -1
+    else: 
+        #neural
+        polarity = 0
+    return polarity
 
 
 def word_freq(label):
     wordsList = []
     for value in label:
         wordsList = wordsList + value
-    freqList = sortFreqDict(wordListToFreqDict(wordsList))[0:15]
+    freqList = sortFreqDict(wordListToFreqDict(wordsList))[0:50]
     print("Total word count length:", len(wordsList))
     print("Top 15 most common words in Label:")
     for i in freqList:
@@ -26,7 +60,7 @@ def word_freq(label):
 
 if __name__ == '__main__':
 
-    df = pd.read_csv('/Users/wangjiale/Desktop/QWQ/Data/500_Reddit_users_posts_labels.csv')
+    df = pd.read_csv('Data/500_Reddit_users_posts_labels.csv')
 
     df = tokenized(df)
 
@@ -35,5 +69,20 @@ if __name__ == '__main__':
     label_2 = df.loc[df['New Label']==2,:]['Tokenized Post']
     label_3 = df.loc[df['New Label']==3,:]['Tokenized Post']
     label_4 = df.loc[df['New Label']==4,:]['Tokenized Post']
+    label_list = [label_0, label_1, label_2, label_3, label_4]
 
-    word_freq(label_0)
+
+    #print(word_freq(label_0))
+
+    for i in label_list:
+        print(word_freq(i))
+
+'''
+    for i in label_0:
+        for j in i: 
+            print(j)
+            print(sentiment_scores(j))
+'''
+  
+    
+    
