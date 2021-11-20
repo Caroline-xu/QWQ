363609a2759495ddc251cbbf6b6f847585ce97fc
Caroline-xu <xuziyu7@gmail.com>
2021-11-20 01:07:24 -0500

    Update punct_cap_feature.py

--- punct_cap_feature.py
+++ punct_cap_feature.py
@@ -1,4 +1,13 @@
 import pandas as pd
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+import statistics
+import scipy.stats as stats
+import pylab as pl
+
+
 import re
 
 def extract_special_feature():
@@ -14,7 +23,24 @@ def extract_special_feature():
     df0["count?"]= df0["Post"].str.count(search2, re.I)
     df0['Uppercase'] = df0['Post'].str.findall(r'[A-Z]').str.len()
     print(df0)
+
+    h1 = np.sort(df0["count!"])
+    mean = statistics.mean(h1)
+    sd = statistics.stdev(h1)
+    plt.plot(h1, norm.pdf(h1, mean, sd), label='for !')
+
+
+    h2 = np.sort(df0["count?"])
+    mean2 = statistics.mean(h2)
+    sd2 = statistics.stdev(h2)
+    plt.plot(h1, norm.pdf(h2, mean2, sd2), label='for ?')
+
+    plt.ylabel('normal distribution')
+    plt.legend()
+    plt.show()
+
+    
     return df0
 
-
+extract_special_feature()
 
