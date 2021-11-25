f6a893f8b8601a595a1b9ff4c558bd214cbf7c07
Caroline-xu <xuziyu7@gmail.com>
2021-11-24 19:04:43 -0500

    Update punct_cap_feature.py

--- punct_cap_feature.py
+++ punct_cap_feature.py
@@ -39,6 +39,55 @@ def extract_special_feature():
     plt.legend()
     plt.show()
 
+    labels = ['Supportive',"Idealtion","Behavior","Indicator","Attempt"]
+    Supportive = 0
+    Ideation = 0
+    Behavior = 0
+    Indicator = 0
+    Attempt = 0
+    sumofexclamation = [0, 0, 0, 0, 0]
+    sumofques = [0, 0, 0, 0, 0]
+    row = len(df0)
+    for i in range(row):
+        if df0['Label'][i] == "Supportive":
+            Supportive += 1
+            sumofexclamation[0] += df0["count!"][i]
+            sumofques[0] += df0["count?"][i]
+        elif df0['Label'][i] == "Ideation":
+            Ideation += 1
+            sumofexclamation[1] += df0["count!"][i]
+            sumofques[1] += df0["count?"][i]
+        elif df0['Label'][i] == "Behavior":
+            Behavior += 1
+            sumofexclamation[2] += df0["count!"][i]
+            sumofques[2] += df0["count?"][i]
+        elif df0['Label'][i] == "Indicator":
+            Indicator += 1
+            sumofexclamation[3] += df0["count!"][i]
+            sumofques[3] += df0["count?"][i]
+        elif df0['Label'][i] == "Attempt":
+            Attempt += 1
+            sumofexclamation[4] += df0["count!"][i]
+            sumofques[4] += df0["count?"][i]
+    
+    Labels = ['Supportive','Ideation','Behavior','Indicator','Attempt']
+    Exclamation_mark_Per_labels = [sumofexclamation[0]/Supportive,sumofexclamation[1]/Ideation,sumofexclamation[2]/Behavior,sumofexclamation[3]/Indicator,sumofexclamation[4]/Attempt]
+    Question_mark_Per_labels = [sumofques[0]/Supportive,sumofques[1]/Ideation,sumofques[2]/Behavior,sumofques[3]/Indicator,sumofques[4]/Attempt]
+
+    X_axis = np.arange(len(Labels))
+
+    plt.bar(X_axis - 0.2, Exclamation_mark_Per_labels, 0.4, label = 'Exclamation Mark (!)')
+    plt.bar(X_axis + 0.2, Question_mark_Per_labels, 0.4, label = 'Question Mark (?)')
+
+    plt.xticks(X_axis, Labels)
+    plt.title('Labels Vs Average Number of Marks in each Label')
+    plt.xlabel('Labels')
+    plt.ylabel('Average Number of Marks in each Label')
+    plt.legend()
+    plt.show()
+    
+
+
     
     return df0
 
