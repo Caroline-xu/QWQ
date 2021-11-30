be827d6312039e6d28cfcc0eca767ea1a4dd8b11
Wanqing Li <71136622+eus-lwq@users.noreply.github.com>
2021-11-30 10:56:39 -0500

    let random forest and decision tree use grid only

--- SplitSet.py
+++ SplitSet.py
@@ -19,12 +19,14 @@ from random_forest import train_random_forest
 # take X as features and Y as label (unnormailzied)
 
 def PCA_reduce_dimension(X, Y):
+    # this function is used to reduce dimension of the data frame to 300 columns
+    # input parameter: X(data frame except label column), Y(label column)
+    # return value: final_df (whole dataframe after PCA reduced dimension to 300 columns)
     X = StandardScaler().fit_transform(X)
     pca = PCA(n_components=300)
     principalComponents = pca.fit_transform(X)
     principalDf = pd.DataFrame(data = principalComponents)
     finalDf = pd.concat([principalDf, Y], axis = 1)
-    #print(finalDf)
     return finalDf
 
 def get_data():
@@ -35,46 +37,52 @@ def get_data():
     #print(df)
     return final_df
 
+def splitSet_forGrid(model_name):
+    # this function will split set and do grid search(do cross validation) for
+    # linear regression and naive bayes
+    # parameter: model_name (e.g. "random_forest"")
+    # output value: model name, accuracy score, macro F1 score, micro F1 score, weighted F1 score
+    X_train, X_test = train_test_split(X, test_size=0.2, random_state=25)
+    y_train, y_test = train_test_split(y, test_size=0.2, random_state=25)
+    if model_name == "random_forest":
+        acc, macroF1,microF1,weightedF1 = train_random_forest(X_train, X_test, y_train, y_test)
+    elif model_name == "decision_tree":
+        acc, macroF1,microF1,weightedF1 = train_decision_tree(X_train, X_test, y_train, y_test) 
+        
+    print("This model is :", model_name)
+    print('Avg accuracy :',"%.2f" %acc)
+    print('Avg macro F1 score :',"%.2f" % macroF1)
+    print('Avg micro F1 score :',"%.2f" % microF1) 
+    print('Avg weighted F1 score :',"%.2f" % weightedF1) 
+
 def fiveCrossValidation(model_name):
+    # this function will split set and do 5 folds cross validation for
+    # linear regression and naive bayes
+    # parameter: model_name (e.g. "linear_regression")
+    # return value: model name, accuracy score, macro F1 score, micro F1 score, weighted F1 score
     k = 5 # 5 fold
     kf = KFold(n_splits=k, random_state=None)
     acc_total = []
-    mse_total = []
-    mae_total = []
-    rmse_total = []
-    #r2Score_total = []
     macroF1_total = []
     microF1_total = [] 
     weightedF1_total = []
-    dummy_total = []
+    #dummy_total = []
     for train_index , test_index in kf.split(X):
         X_train , X_test = X.iloc[train_index,:],X.iloc[test_index,:]
         y_train , y_test = y[train_index] , y[test_index]
         
         if model_name == 'linear_regression':
-            acc,mse,mae,rmse,macroF1,microF1,weightedF1,dummy = train_linear_regression(X_train, X_test, y_train, y_test)
-        elif model_name == 'decision_tree':
-            acc,mse,mae,rmse,macroF1,microF1,weightedF1,dummy = train_decision_tree(X_train, X_test, y_train, y_test)
+            acc,macroF1,microF1,weightedF1 = train_linear_regression(X_train, X_test, y_train, y_test)
         elif model_name == 'naive_bayes':
-            acc,mse,mae,rmse,macroF1,microF1,weightedF1,dummy = train_naive_bayes(X_train, X_test, y_train, y_test)
-        elif model_name == 'random_forest':
-            acc,mse,mae,rmse,macroF1,microF1,weightedF1,dummy = train_random_forest(X_train, X_test, y_train, y_test)
-
+            acc,macroF1,microF1,weightedF1 = train_naive_bayes(X_train, X_test, y_train, y_test)
+            
         acc_total.append(acc)
-        mse_total.append(mse)
-        mae_total.append(mae)
-        rmse_total.append(rmse)
-        #r2Score_total.append(r2Score)
         macroF1_total.append(macroF1)
         microF1_total.append(microF1)
         weightedF1_total.append(weightedF1)
-        dummy_total.append(dummy)
+        #dummy_total.append(dummy)
         
     avg_acc = sum(acc_total)/k
-    avg_mse = sum(mse_total)/k
-    avg_mae = sum(mae_total)/k
-    avg_rmse = sum(rmse_total)/k
-    #avg_r2Score = sum(r2Score_total)/k 
     avg_macroF1 = sum(macroF1_total)/k
     avg_microF1 = sum(microF1_total)/k 
     avg_weightedF1 = sum(weightedF1_total)/k
@@ -83,14 +91,12 @@ def fiveCrossValidation(model_name):
     #print('accuracy of each fold - {}'.format(acc_total))
     print("This model is :", model_name)
     print('Avg accuracy :',"%.2f" %avg_acc)
-    print('Avg mse :',"%.2f" %avg_mse)
-    print('Avg mae :',"%.2f" %avg_mae)
-    print('Avg rmse :',"%.2f" % avg_rmse)
+
     #print('Avg r2 score :',"%.2f" % avg_r2Score)
     print('Avg macro F1 score :',"%.2f" % avg_macroF1)
     print('Avg micro F1 score :',"%.2f" % avg_microF1) 
     print('Avg weighted F1 score :',"%.2f" % avg_weightedF1) 
-    print('dummy total:', dummy_total)
+    #print('dummy total:', dummy_total)
     #print('Avg dummy:',"%.2f" % avg_dummy)  
     
 # Press the green button in the gutter to run the script.
@@ -107,11 +113,16 @@ if __name__ == '__main__':
     
     ####train model by using 5 fold cross validation####
     # 2- Cross-fold validation on the train data   
-    model_name = "random_forest"
+    
+    #model_name = "random_forest"
     #model_name = "linear_regression"
     #model_name = "naive_bayes"
-    #model_name = "decision_tree"
-    fiveCrossValidation(model_name)  
+    model_name = "decision_tree"
+    
+    if model_name == "random_forest" or "decision tree":
+        splitSet_forGrid(model_name) 
+    elif model_name == "linear_regression" or "naive_bayes":
+        fiveCrossValidation(model_name)  
 
 
     
