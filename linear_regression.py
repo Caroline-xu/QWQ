6377fbfb8b7b2d90743581e8693ec1cca113c30a
Wanqing Li <71136622+eus-lwq@users.noreply.github.com>
2021-11-30 10:58:30 -0500

    deleted rmse, mae, mse

--- linear_regression.py
+++ linear_regression.py
@@ -1,6 +1,5 @@
 from sklearn import linear_model
-from sklearn.metrics import mean_squared_error, mean_absolute_error, accuracy_score
-from matplotlib import pyplot as plt
+from sklearn.metrics import accuracy_score
 import numpy as np
 from dummy_classifier import dummy_classifier
 from sklearn.metrics import f1_score
@@ -18,23 +17,13 @@ def train_linear_regression(X_train, X_test, y_train, y_test):
     #accuracy method 1: really high accuracy 80-90
     #accuracyRounded = lg_model.score(X_test, y_pred_round)
     #accuracy method 2: really low accuracy 9 -13
-    
     acc = accuracy_score(y_test, y_pred_round)
-    #print("accuracy score of Linear regression:", accuracyScore)
-    #get MSE mean squared error
-    mse = mean_squared_error(y_test,y_pred_round)
-    #print("mean squared error of Linear regression:", mse)
-    #get MAE
-    mae = mean_absolute_error(y_test, y_pred_round)
-    #print("mean absolute error of Linear regression:", mae)
-    #get RMSE
-    rmse = np.sqrt(mae)
-    #r2Score=r2_score(y_test, y_pred)
+    
     macroF1=f1_score(y_test, y_pred_round, average='macro')
     microF1=f1_score(y_test, y_pred_round, average='micro')
     weightedF1=f1_score(y_test, y_pred_round, average='weighted')
-    dummy=dummy_classifier(X_train, y_train)
-    return acc, mse, mae, rmse, macroF1,microF1,weightedF1,dummy
+    #dummy=dummy_classifier(X_train, y_train)
+    return acc, macroF1,microF1,weightedF1
    
 
 
