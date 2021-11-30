8859c4a6cae54150162137bbebadc8d5aa2b997d
Wanqing Li <71136622+eus-lwq@users.noreply.github.com>
2021-11-30 10:59:10 -0500

    deleted rmse,mse,mae

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,11 +1,7 @@
 def train_naive_bayes(X_train, X_test, y_train, y_test):
-    
-    from sklearn import datasets
+
     from sklearn.naive_bayes import GaussianNB
-    from sklearn.model_selection import GridSearchCV
-    import pandas as pd
-    import numpy as np
-    from sklearn.metrics import mean_squared_error, mean_absolute_error, accuracy_score
+    from sklearn.metrics import accuracy_score
     from sklearn.metrics import f1_score
     from dummy_classifier import dummy_classifier
     #because naive bayes has no parameter to tune, so it could not use grid search
@@ -15,20 +11,11 @@ def train_naive_bayes(X_train, X_test, y_train, y_test):
     acc = accuracy_score(y_test,y_pred)
     #print("Detailed classification report:")
     #print(classification_report(y_test, y_pred))
-    acc = accuracy_score(y_test,y_pred)# need to change after this
-    mse = mean_squared_error(y_test,y_pred)
-    mae = mean_absolute_error(y_test, y_pred)
-    rmse = np.sqrt(mae)
+    acc = accuracy_score(y_test,y_pred)
+
     #r2Score=r2_score(y_test, y_pred)
     macroF1=f1_score(y_test, y_pred, average='macro')
     microF1=f1_score(y_test, y_pred, average='micro')
     weightedF1=f1_score(y_test, y_pred, average='weighted')
-    dummy=dummy_classifier(X_train, y_train)
-    return acc, mse, mae, rmse, macroF1,microF1,weightedF1,dummy
-
-    #acc = np.sum(y_test == y_pred) / X_test.shape[0]
-    #print("Test Acc : %.3f" % acc)
-    # predict the model
-    #y_proba = gnb.predict_proba(X_test[:1])
-    #print(gnb.predict(X_test[:1]))
-    #print("The estimated probability:", y_proba)
+    #dummy=dummy_classifier(X_train, y_train)
+    return acc, macroF1,microF1,weightedF1
